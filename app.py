4e06dd66566cf7475bcca2cfdf36483a54775b50
yuganshsuri <suriyugansh2005@gmail.com>
2026-06-25 13:52:11 +0530

    backend connected using flas project.html to be uploaded soon

--- app.py
+++ app.py
@@ -1,2 +1,20 @@
-from flask import Flask,url_for,render_template,request
+from flask import Flask,render_template,url_for,request
 app=Flask(__name__)
+@app.route('/')
+def index():
+    return render_template("index.html")
+@app.route("/about")
+def about():
+    return render_template ("about.html")
+@app.route("/history")
+def history():
+    return render_template ("history.html")
+@app.route("/contact")
+def contact():
+    return render_template ("contact.html")
+# @app.route('/project')
+# def predict():
+# data=["Age","Gender","BMI","BloodPressure","GlucoseLevel","Cholesterol","HeartRate","Smoking","Alcohol","PhysicalActivity","FamilyHistory"]
+#     return render_template ("project.html")
+if __name__=="__main__":
+    app.run()
